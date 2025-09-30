3326f8c9b5aa3db0c9cd2d454d42e6db44b290aa
kkaturi14 <kkaturi14@gmail.com>
2025-09-30 16:20:48 +0900

    fix

--- src/app/dashboard-c/page.tsx
+++ src/app/dashboard-c/page.tsx
@@ -1,25 +1,25 @@
 'use client'
 import { useUser } from '@clerk/nextjs'
-import React from 'react'
 
-export default function Page() {
-  const { isSignedIn, isLoaded } = useUser()
+export default function DashboardCPage() {
+  const { isSignedIn, user, isLoaded } = useUser()
 
   if (!isLoaded) {
-    return <div>로딩중........</div>
+    return <div>Loading...</div>
   }
 
   if (!isSignedIn) {
-    return (
-      <div>Sign in to view this page. 이 페이지를 보려면 로그인하세요.</div>
-    )
+    return <div>Sign in to view this page</div>
   }
 
   return (
     <div>
       <h1 className="text-2xl font-bold mb-5">Dashboard (Client-side)</h1>
       <div className="mb-5">
-        <p></p>
+        <p>use client 지시자 사용함 </p>
+        <p>브라우저에서 useUser() 함수 이용</p>
+        <p>Welcome, {user?.firstName}!</p>
+        <p>Email: {user?.primaryEmailAddress?.emailAddress}</p>
       </div>
     </div>
   )
